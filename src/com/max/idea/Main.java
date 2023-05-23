55520ed7eb17fcab4bf49798db01449bdef8f02e
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-05-23 11:40:38 +0300

    - задание 3

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -2,6 +2,9 @@ package com.max.idea;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] fiveDigitArray = new int[] {10, 20, 30, 40, 50};
+        fiveDigitArray[0] = 50;
+        fiveDigitArray[(fiveDigitArray.length - 1)] = 10;
+        System.out.println(fiveDigitArray[0] + fiveDigitArray[(fiveDigitArray.length - 3)]);
     }
 }

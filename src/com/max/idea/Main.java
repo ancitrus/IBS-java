0f0282de21c8bc8ccbdfe4dec3e198ee316a24b6
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-05-23 15:07:10 +0300

    - задание 4

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,23 @@
 package com.max.idea;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите первое число:");
+        double x = Double.parseDouble(br.readLine());
+        System.out.println("Введите второе число:");
+        double y = Double.parseDouble(br.readLine());
+        System.out.println("Введите третье число:");
+        double z = Double.parseDouble(br.readLine());
+        double avg = (x + y + z) / 3.0;
+        System.out.println("Среднее арифметическое трех чисел: " + avg);
+        double halfAvg = Math.floor(avg / 2);
+        if (halfAvg > 3) {
+            System.out.println("Программа выполнена корректно");
+        }
     }
 }

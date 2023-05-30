04e3162dc2d43249de96e5edf1ce4d8fd628e7be
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-05-30 22:22:18 +0300

    - задание 5

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,31 @@
 package com.max.idea;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double num1;
+        double num2;
+        double answer;
+        char operator;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Введите 2 числа: ");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("\nВведите один из символов ‘+’, ‘-’, ‘*’ или ‘/’ : ");
+        operator = reader.next().charAt(0);
+        switch(operator) {
+            case '+': answer = num1 + num2;
+                break;
+            case '-': answer = num1 - num2;
+                break;
+            case '*': answer = num1 * num2;
+                break;
+            case '/': answer = num1 / num2;
+                break;
+            default:  System.out.print("Введите корректный оператор!");
+                return;
+        }
+        System.out.print("\nРезультат выполнения соответствующей арифметической операции:\n");
+        System.out.print(num1 + " " + operator + " " + num2 + " = " + answer);
     }
 }

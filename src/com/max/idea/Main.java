ba422940ef72a2cd0619c5c396cde6b1c31ad8b5
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-07 00:11:47 +0300

    - задание 10

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,27 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i, j, array_length, array_width;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите размер двумерного массива");
+        array_length = reader.nextInt();
+        array_width = reader.nextInt();
+        int[][] user_array = new int[array_length][array_width];
+
+        System.out.println("Заполните массив значениями");
+        for (i = 0; i < array_length; i++) {
+            for (j = 0; j < array_width; j++) {
+                user_array[i][j] = reader.nextInt();
+            }
+        }
+
+        for (j=0; j<user_array[0].length; j++){
+            System.out.println(user_array[0][j] * 3);
+        }
     }
 }

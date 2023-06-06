f63dcb2060f4239d13000edd60c69c8f94523336
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-06 22:38:06 +0300

    - задание 8

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,19 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n, i, sum;
+        System.out.println("Введите любое целое положительное число");
+        Scanner reader = new Scanner(System.in);
+        n = reader.nextInt();
+        sum = 0;
+
+       for (i=1; i<=n; i++) {
+           sum = sum + i;
+       }
+        System.out.println(sum);
     }
 }

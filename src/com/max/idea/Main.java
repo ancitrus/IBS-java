244d0f8d75e4498b98fb45151d9b988a6cd49fa5
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-06 23:21:55 +0300

    - задание 9

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,28 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int array_length;
+        double sum = 0;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите размер массива");
+        array_length = reader.nextInt();
+        double[] user_array = new double[array_length];
+
+        System.out.println("\nВведите данные с клавиатуры в массив");
+        for (int i = 0; i < array_length; i++) {
+            System.out.print("\n" + (i+1) + ": ");
+            user_array[i] = reader.nextDouble();
+        }
+
+        for (int i=0; i < array_length; i++){
+            sum = sum + user_array[i];
+        }
+        System.out.println("Среднее значение равно " + (sum/array_length));
     }
 }

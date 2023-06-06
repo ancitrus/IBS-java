0e4115e42d10334bbfe4b4a1d3469f03fdbb3076
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-06 22:17:44 +0300

    - задание 7

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,30 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static final int X = 2;
+    public static final int Y = 33;
+    public static final int Z = 100;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int array_length, i;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите размер массива");
+        array_length = reader.nextInt();
+        int[] user_array = new int[array_length];
+
+        System.out.println("\nВведите данные с клавиатуры в массив");
+        for (i = 0; i < array_length; i++) {
+            System.out.print("\n" + (i+1) + ": ");
+            user_array[i] = reader.nextInt();
+
+            if (user_array[i] == X || user_array[i] == Y || user_array[i] == Z) {
+                System.out.println("Данное значение " + user_array[i]+ " имеется в константах");
+            }
+        }
     }
 }

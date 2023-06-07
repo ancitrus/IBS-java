7a51c42d4d174acf9d3c0e6ef976979d254b5e24
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-07 22:35:30 +0300

    - задание 12

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -2,6 +2,17 @@ package com.max.idea;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String STR = "I like Java!!!";
+
+        if(STR.contains("Java") || STR.startsWith("I like") || STR.endsWith("!!!")) {
+            System.out.println("Строка содержит подстроку \"Java\", начинается с  \"I like\" и " +
+                    "заканчивается на \"!!!\"");
+            System.out.println(STR.toUpperCase());
+        } else {
+            System.out.println("Строка не содержит подстроку \"Java\" или не начинается с  \"I like\" или " +
+                    "не заканчивается на \"!!!\"");
+        }
+        String result = STR.replaceAll("a", "o");
+        System.out.println(result.substring(7, 11));
     }
 }

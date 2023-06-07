cd9800181935703dcdf8d68be9c6996d39766e2e
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-07 20:29:55 +0300

    - задание 11

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,32 @@
 package com.max.idea;
 
+import java.util.Scanner;
+import java.lang.Integer;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int int_var;
+        String str_var;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите первое число");
+        str_var = Integer.toString(reader.nextInt());
+
+        System.out.println("Введите второе число");
+        int_var = reader.nextInt();
+
+        int cmp = Integer.valueOf(str_var).compareTo(int_var);
+        if (cmp > 0){
+            System.out.println("Большее число равно: " + str_var);
+            System.out.println("Меньшее число равно: " + (double) int_var);
+        } else if (cmp < 0){
+            System.out.println("Большее число равно: " + int_var);
+            System.out.println("Меньшее число равно: " + Double.valueOf(str_var));
+        } else {
+            System.out.println("Числа равны");
+        }
     }
 }

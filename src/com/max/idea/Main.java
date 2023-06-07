00664e97f858ba7486b5a31b945c45bb7ffc70e1
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-08 00:17:28 +0300

    - задание 13

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,35 @@
 package com.max.idea;
 
+
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringJoiner latins = new StringJoiner(", ");
+        String user_input;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите строку из слов, разделенных пробелами");
+        user_input = reader.nextLine();
+
+        String[] words = user_input.split(" ");
+
+        int cnt = 0;
+        for (String word : words) {
+            if (word.matches("\\b[A-Za-z]+\\b")) {
+                latins.add(word);
+                cnt++;
+            }
+        }
+
+        if(cnt > 0)
+            System.out.println("\nЛатинские слова: " + latins);
+        System.out.println("\nКоличество латинских слов: " + cnt);
     }
 }

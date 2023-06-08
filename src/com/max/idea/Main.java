9a84ce986050e4532907ba28457eec6dcd118fdc
Анна Сергеева <AKonSergeeva@sberbank.ru>
2023-06-08 18:44:25 +0300

    - итоговое задание 1

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,20 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double roubles, currency, dollars;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите колиество рублей");
+        roubles = reader.nextDouble();
+
+        System.out.println("Введите курс доллара");
+        currency = reader.nextDouble();
+
+        dollars = roubles / currency;
+        System.out.println("Количество долларов: " + String.format("%.2f", dollars));
     }
 }
